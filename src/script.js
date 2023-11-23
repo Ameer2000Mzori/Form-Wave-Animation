bd10bc9b07175bd28d9561e89030d02c823aa6c5
Ameer <ameerapex496@gmail.com>
2023-11-23 18:12:44 +0100

    added and merged form

--- src/script.js
+++ src/script.js
@@ -1,11 +1,9 @@
 var labels = document.querySelectorAll(".form-control label");
 labels.forEach(function (label) {
-  label.innerHTML = label.innerText
-    .split("")
-    .map(function (letter, i) {
-      return '<span style="transition-delay: '
-        .concat(i * 50, 'ms">')
-        .concat(letter, "</span>");
+    label.innerHTML = label.innerText
+        .split("")
+        .map(function (letter, i) {
+        return "<span style=\"transition-delay: ".concat(i * 50, "ms\">").concat(letter, "</span>");
     })
-    .join("");
+        .join("");
 });
